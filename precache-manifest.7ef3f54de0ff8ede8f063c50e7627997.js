5823acc410f1eeab2a5e8e59b4e2036397b9eed4
Jane Philipps <jane.philipps@gmail.com>
2019-01-13 10:25:46 -0800

    Updates

---
+++ precache-manifest.7ef3f54de0ff8ede8f063c50e7627997.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40aae076b4f75278287a",
+    "url": "/every-second-a-color-2/static/css/main.1562cc84.chunk.css"
+  },
+  {
+    "revision": "40aae076b4f75278287a",
+    "url": "/every-second-a-color-2/static/js/main.40aae076.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/every-second-a-color-2/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "d3c328525b6dfb6e147d",
+    "url": "/every-second-a-color-2/static/js/runtime~main.d3c32852.js"
+  },
+  {
+    "revision": "0f31cbd0b576562ca7419252ac369253",
+    "url": "/every-second-a-color-2/index.html"
+  }
+];
